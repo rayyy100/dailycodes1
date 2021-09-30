996e6ed3352fca71001d81d8737bdc0cde20c72e
rayyy100 <91695098+rayyy100@users.noreply.github.com>
2021-09-30 13:06:58 -0600

    Create age.money.cpp

---
+++ age.money.cpp
@@ -0,0 +1,30 @@
+//introduction to variables in C++
+
+#include<iostream>   
+using namespace std;
+int main() {
+
+
+	int pet = 30;
+	int age = 17;
+	char initial1 = 'r';
+	char initial2 = 'c';
+	float money = 1313438413513851351435435453584358413.03;
+
+	cout << "Hey!" << endl;
+
+	cout << "I see that you are " << age << "Years Old" << endl;
+	cout << " The drivers Inital is " << initial1 << " and the nav's Initial is " << initial2 << " YOU HAVE " <<  money  << " Dollars " <<" YOU ARE SOOOOO RICH!!!!!!!!!!!! " << endl;
+	cout << " I have " << pet << " pets ";
+
+
+
+
+
+
+
+
+
+
+
+}
