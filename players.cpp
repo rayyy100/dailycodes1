4de9ccf2e12d9a903de347b09e07c724afea82af
rayyy100 <91695098+rayyy100@users.noreply.github.com>
2021-09-30 13:06:07 -0600

    Create players.cpp

---
+++ players.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int p;
+	int x;
+	int pc; 
+	cout << "how many playstation players are there?" << endl; 
+	cin >> p;
+	cout << "how many nintendo players are there?" << endl;
+	cin >> n;
+	cout << "how many xbox players are there?" << endl;
+	cin >> x;
+	cout << "how many pc games players are there?" << endl;
+	cin >> pc;
+	for (int i = 0; i < p; i++) 
+		cout << "*";
+	cout << endl;
+	for (int i = 0; i < n; i++) 
+		cout << "*";
+	cout << endl;
+	for (int i = 0; i < x; i++)
+		cout << "*";
+	cout << endl;
+	for (int i = 0; i < pc; i++)
+		cout << "*";
+	cout << endl;
+
+
+
+
+
+
+
+
+
+}
